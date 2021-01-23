cf4c7725e7e277163e0a86f9fb7f958ab57fb7e7
Hélder Gonçalves <helderpimentaxc@gmail.com>
2021-01-23 23:27:39 +0000

    refactor code

--- src/util/MyButton.js
+++ src/util/MyButton.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
+import Tooltip from "@material-ui/core/Tooltip";
+import IconButton from "@material-ui/core/IconButton";
 //para poder utilizar em varias paginas sem estar a repetir codigo
-              //children = o q esta dentro // tip e o titulo pro tooltip
+//children = o q esta dentro // tip e o titulo pro tooltip
 export default ({ children, onClick, tip, btnClassName, tipClassName }) => (
   <Tooltip title={tip} className={tipClassName} placement="top">
     <IconButton onClick={onClick} className={btnClassName}>
